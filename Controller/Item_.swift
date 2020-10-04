439c56cff50bfee246b2e1a520b2320c642a7a41
AngelaTse <angela4794@icloud.com>
2020-10-04 21:58:49 +0800

    - add function: mark as important

--- Controller/Item_.swift
+++ Controller/Item_.swift
@@ -4,5 +4,6 @@ import RealmSwift
 class Item_ : Object {
     @objc dynamic var issue : String = "";
     @objc dynamic var isDone: Bool = false;
+    @objc dynamic var isImportant: Bool = false;
     @objc dynamic var ownerList: String?
 }
--- Controller/TodoListViewController.swift
+++ Controller/TodoListViewController.swift
@@ -14,6 +14,7 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
 
     let defaults = UserDefaults.standard;
     let realm = try! Realm();
+    let important_img = UIImageView();
     var all_items: Results<Item_>!;
     var selected_items : Results<Item_>!;
     var lists: Results<todoList>!;
@@ -46,6 +47,9 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
         self.tableView.backgroundColor = UIColor(red: 1.0, green: 0.99, blue: 0.99, alpha: 1.0)
         print("folder of Realm,\(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))");
 
+        important_img.frame = CGRect(x: 0, y: 0, width: 20, height: 20);
+        important_img.image = UIImage(named:"heart-solid.jpg");
+        
         
 //       if let temp = defaults.array(forKey: "tempArray") as? [arrayItem] {tempArray = temp;}
     }
@@ -61,6 +65,7 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
             cell.delegate = self;
             cell._todoLabel?.text = self.selected_items[indexPath.row].issue;
             cell._tickBox.image = selected_items[indexPath.row].isDone ? UIImage(named: "check-square-gray.png") : UIImage(named: "square-regular.png");
+            cell.accessoryView = selected_items[indexPath.row].isImportant ? important_img : .none;
             cell._todoLabel.textColor =  selected_items[indexPath.row].isDone ? UIColor(red: 0.672, green: 0.675, blue: 0.706, alpha: 1.0) : UIColor.black;
             return cell;
         }
@@ -108,17 +113,50 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     //swipe to delete cell
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            do { try self.realm.write {
+//                self.realm.delete(self.selected_items[indexPath.row])
+//                }}
+//            catch { print("delete row error,\(error)")};
+//            self.tableView.deleteRows(at: [indexPath], with: .none);
+//            self.tableView.reloadData();
+//        }
+//        }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+           let delete = UIContextualAction(style: .normal, title: NSLocalizedString("delete", comment: "")) { (action, view, completion) in
             do { try self.realm.write {
                 self.realm.delete(self.selected_items[indexPath.row])
                 }}
-            catch { print("delete row error,\(error)")};
-            self.tableView.deleteRows(at: [indexPath], with: .none);
+                catch { print("delete row error,\(error)")};
+                self.tableView.deleteRows(at: [indexPath], with: .none);
+                self.tableView.reloadData();
+                   
+               completion(true)
+           }
+           delete.backgroundColor =  UIColor(red: 0.83, green: 0.43, blue: 0.5, alpha: 1.0)
+        
+        let important = UIContextualAction(style: .normal, title: NSLocalizedString("markAsImportant", comment: "")) { (action, view, completion) in
+            do { try self.realm.write {
+                self.selected_items[indexPath.row].isImportant = !self.selected_items[indexPath.row].isImportant;
+                }
+            }
+            catch {print("did select row errer,\(error)")}
             self.tableView.reloadData();
+                
+            completion(true)
         }
-        }
-
+        important.backgroundColor =  UIColor(red: 0.98, green: 0.72, blue: 0.42, alpha: 1.0)
+        
+        
+        let config = UISwipeActionsConfiguration(actions: [delete, important]);
+        config.performsFirstActionWithFullSwipe = false;
+        
+           return config
+       }
+    
     
     func initNavBar(){
         navBarBtn.frame = CGRect(x: 0, y: 0, width: 300, height: 35)
@@ -268,32 +306,3 @@ class TodoListViewController: UIViewController, UITableViewDelegate, UITableView
         self.listPicker.reloadAllComponents();
     }
 }
-
-//todo trailing action
-//    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//
-//        let delete = UIContextualAction(style: .normal, title: "Delete") { (action, view, completion) in
-//            do { try self.realm.write {
-//                self.realm.delete(self.items[indexPath.row])
-//                }}
-//            catch { print(error)};
-//            self.tableView.deleteRows(at: [indexPath], with: .none);
-//            self.tableView.reloadData();
-//            completion(true)
-//        }
-//        delete.backgroundColor =  UIColor(red: 0.991, green: 0.184, blue: 0.462, alpha: 1.0)
-//
-//        let edit = UIContextualAction(style: .normal, title: "Added") { (action, view, completion) in
-//        let cell = self.tableView.cellForRow(at: indexPath) as! TodoListTableViewCell;
-//        cell._todoLabel.isEnabled = true;
-//            self.tableView.isEditing = true;
-//            completion(false)
-//        }
-//        edit.image = UIImage(named: "Small Image")
-//        edit.backgroundColor =  UIColor(red: 0.2436070212, green: 0.5393256153, blue: 0.1766586084, alpha: 1)
-//
-//        let config = UISwipeActionsConfiguration(actions: [delete, edit])
-//        config.performsFirstActionWithFullSwipe = false
-//
-//        return config
-//    }
