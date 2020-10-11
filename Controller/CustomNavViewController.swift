c4d80f8c1a23439afc3c9bdca374b0f19f5a2cee
AngelaTse <angela4794@icloud.com>
2020-10-11 22:48:03 +0800

    - remove modal of colorViewPicker

--- Controller/CustomNavViewController.swift
+++ Controller/CustomNavViewController.swift
@@ -5,10 +5,6 @@ class CustomNavViewController: UINavigationController, changeNavBarColorDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let colorPickerView = storyboard.instantiateViewController(withIdentifier: "ColorPickerView") as? ColorPickerViewController
-        colorPickerView!.delegate = self;
-        present(colorPickerView!, animated: true, completion: nil)
         self.navigationBar.barTintColor = UIColor(red: 0.8, green: 0.61, blue: 0.01, alpha: 1.0);
     }
     
